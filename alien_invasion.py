f146114e54fdb26e91fddba4fdcd0293aaabe82d
sammy saqfelhait <ssaqfelhait@student.cscc.edu>
2026-04-08 22:19:57 -0400

    created int game loop and set up display

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,21 @@
 import sys
 import pygame
 
+class AlienInvasion:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            self.screen.fill((230, 230, 230))
+
+            pygame.display.flip()
+
 if __name__ == '__main__':
     pass
